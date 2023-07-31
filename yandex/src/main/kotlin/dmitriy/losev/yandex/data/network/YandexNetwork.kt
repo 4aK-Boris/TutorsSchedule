201b60d543b867c9bc7018e551cd24a23545a014
dmitriy <dmitriylosevxxx@gmail.com>
2023-07-31 15:37:20 +0300

    Доделан модуль аунтификации через Яндекс ID

--- yandex/src/main/kotlin/dmitriy/losev/yandex/data/network/YandexNetwork.kt
+++
@@ -1,8 +0,0 @@
-package dmitriy.losev.yandex.data.network
-
-import dmitriy.losev.yandex.data.dto.YandexUserDataDTO
-
-interface YandexNetwork {
-
-    suspend fun getUserData(token: String): YandexUserDataDTO
-}
--- yandex/src/test/kotlin/dmitriy/losev/yandex/ExampleUnitTest.kt
+++ yandex/src/test/kotlin/dmitriy/losev/yandex/ExampleUnitTest.kt
@@ -1,16 +1,13 @@
 package dmitriy.losev.yandex
 
-import org.junit.Assert
-import org.junit.Test
+import org.koin.test.KoinTest
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        Assert.assertEquals(4, 2 + 2)
-    }
+class ExampleUnitTest: KoinTest {
+
+
 }
